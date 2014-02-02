923aa5f65dbafad77734bb95be18d4d884051ebb
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2014-02-02 14:01:23 -0500

    Initial Commit

---
+++ KZBookshelf/KZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KZAppDelegate.h
+//  KZBookshelf
+//
+//  Created by kyoshikawa on 14/2/2.
+//  Copyright (c) 2014 Electricwoods LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KZBookshelf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KZBookshelf
+//
+//  Created by kyoshikawa on 14/2/2.
+//  Copyright (c) 2014 Electricwoods LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([KZAppDelegate class]));
+	}
+}
